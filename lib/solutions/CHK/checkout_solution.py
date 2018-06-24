6741c9874e3db2fd210e40b2bba411ec5e0f34ce
sbx_user1061 <sbx_user1061@ip-10-12-112-10.eu-west-2.compute.internal>
2018-06-24 15:47:32 +0000

    Sun Jun 24 15:47:32 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,10 +4,13 @@
 # skus = unicode string
 def checkout(skus):
     sku_set = set(skus)
-    print sku_set
     if sku_set.difference((['A', 'B', 'C', 'D'])):
         return -1
-    #raise NotImplementedError()
+    basket_count = {'A': 0, 'B': 0, 'C': 0, 'D':0}
+    for letter in skus:
+        basket_count[letter]+=1
+    return basket_count
+
 
 
 if __name__ == '__main__':
