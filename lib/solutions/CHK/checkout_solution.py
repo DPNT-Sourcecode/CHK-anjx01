aac8541ec9101adc4a14c6344b11c829c44364ad
sbx_user1061 <sbx_user1061@ip-10-12-112-10.eu-west-2.compute.internal>
2018-06-24 16:55:02 +0000

    Sun Jun 24 16:55:02 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,12 +13,14 @@ def checkout(skus):
         basket_count[letter]+=1
 
     total_val = 0
-    price_model = {'A': {'single_pricing': 50,{'multi_num': 3, 'multi_pricing': 130, 'extra': None, 'extra_num': None},
-                                              {'multi_num': 5, 'multi_pricing': 200, 'extra': None, 'extra_num': None}},
+    price_model = {'A': {'single_pricing': 50,[{'multi_num': 5, 'multi_pricing': 200, 'extra': None, 'extra_num': None},
+                                                {'multi_num': 3, 'multi_pricing': 130, 'extra': None, 'extra_num': None}]},
                    'B': {'single_pricing': 30, 'multi_num': 2, 'multi_pricing': 45, 'extra': None, 'extra_num': None}, 
                    'C': 20,
                    'D': 15,
                    'E': {'single_pricing': 40, {'multi_num':2, 'multi_pricing': 80, 'extra': 'B', 'extra_num': 1 }}}
+
+    order = ['E','A','B','C','D']
 
     for sku in basket_count:
         num_items = basket_count[sku]
@@ -29,6 +31,11 @@ def checkout(skus):
             pricing = price_model[sku]
             total_val+= num_items/pricing['multi_num']*pricing['multi_pricing']+\
             num_items%pricing['multi_num']*pricing['single_pricing']
+            if pricing['extra']:
+                basket_count['extra']-= num_items/pricing['multi_num']
+
+        if type(pricing)==list:
+
         else:
 
             total_val+= num_items*pricing
