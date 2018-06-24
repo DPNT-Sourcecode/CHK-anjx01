450cb84603e90531b2c1f88d59ee11e31c7cecdd
sbx_user1061 <sbx_user1061@ip-10-12-112-10.eu-west-2.compute.internal>
2018-06-24 15:01:20 +0000

    Sun Jun 24 15:01:20 UTC 2018

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -3,11 +3,13 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
     try:
-        if x or y not in range(101):
+        if (x or y) not in range(101):
             return 'hello'
-
-        return x+y
+        else:
+            return x+y
     except TypeError:
         print 'x or y are not integers'
 
 
+if __name__ == '__main__':
+    print compute(1,2)
