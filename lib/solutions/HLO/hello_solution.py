c4b7369a7c96316df619fd3503360244024abc0d
sbx_user1061 <sbx_user1061@ip-10-12-112-10.eu-west-2.compute.internal>
2018-06-24 15:20:06 +0000

    Sun Jun 24 15:20:06 UTC 2018

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,4 +3,7 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    raise NotImplementedError()
+    if type(friend_name)!=str:
+        raise TypeError 
+    return 'Hello World'
+
