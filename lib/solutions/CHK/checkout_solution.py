06b4da0921cfccac94c8101f4cd4df14dd02eef8
sbx_user1061 <sbx_user1061@ip-10-12-112-10.eu-west-2.compute.internal>
2018-06-24 16:17:32 +0000

    Sun Jun 24 16:17:32 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -15,17 +15,21 @@ def checkout(skus):
                    'B': {'single_pricing': 30, 'multi_num': 2, 'multi_pricing': 45}, 
                    'C': 20,
                    'D': 15}
+    print basket_count
     for sku in basket_count:
         num_items = basket_count[sku]
         pricing = price_model[sku]
+        print sku,
         if len(pricing)==3:
+            print num_items/pricing['multi_num']*pricing['multi_pricing'], num_items%pricing['multi_num']*pricing['single_pricing']
             pricing = price_model[sku]
             total_val+= num_items/pricing['multi_num']*pricing['multi_pricing']+\
             num_items%pricing['multi_num']*pricing['single_pricing']
         else:
+            print num_items*pricing
             total_val+= num_items*pricing
 
         return total_val
 
 if __name__ == '__main__':
-    print checkout('AAAAAAA')
+    print checkout('ABCA')
