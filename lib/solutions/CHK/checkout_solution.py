e9555cfba752f481d9723e18cb25d979953d3b74
sbx_user1061 <sbx_user1061@ip-10-12-112-10.eu-west-2.compute.internal>
2018-06-24 15:41:32 +0000

    Sun Jun 24 15:41:32 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,5 +3,11 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    sku_set = set(sku.split())
-    raise NotImplementedError()
+    sku_set = set(skus)
+    if sku_set!={'A', 'B', 'C', 'D'}:
+        return -1
+    #raise NotImplementedError()
+
+
+if __name__ == '__main__':
+    print checkout('A')
