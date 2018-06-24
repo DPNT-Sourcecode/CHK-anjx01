afa0bd5b507ce9a864f95fbe458000baf4781692
sbx_user1061 <sbx_user1061@ip-10-12-112-10.eu-west-2.compute.internal>
2018-06-24 17:38:38 +0000

    Sun Jun 24 17:38:38 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,9 +6,9 @@ def checkout(skus):
     if type(skus)==list:
         skus = ''.join(skus)
     sku_set = set(skus)
-    if sku_set.difference((['A', 'B', 'C', 'D', 'E', 'F'])):
+    if sku_set.difference((['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])):
         return -1
-    basket_count = {'A': 0, 'B': 0, 'C': 0, 'D':0, 'E':0, 'F':0}
+    basket_count = {'A': 0, 'B': 0, 'C': 0, 'D':0, 'E':0, 'F':0, 'G':0, 'H':0, 'I':0, 'J':0, 'K':0, 'L':0, 'M':0, 'N':0, 'O':0, 'P':0, 'Q':0, 'R':0, 'S':0, 'T':0, 'U':0, 'V':0, 'W':0, 'X':0, 'Y':0, 'Z':0}
     for letter in skus:
         basket_count[letter]+=1
 
