105a11394afc7c8b220486204e5eb691710d0824
sbx_user1061 <sbx_user1061@ip-10-12-112-10.eu-west-2.compute.internal>
2018-06-24 16:05:32 +0000

    Sun Jun 24 16:05:32 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,7 +9,13 @@ def checkout(skus):
     basket_count = {'A': 0, 'B': 0, 'C': 0, 'D':0}
     for letter in skus:
         basket_count[letter]+=1
-    return basket_count
+
+    total_val = 0
+    price_model = {'A': {'single_pricing': 50, 'multi_num': 3, 'multi_pricing': 130},
+                   'B': {'single_pricing': 30, 'multi_num': 2, 'multi_pricing': 45}, 
+                   'C': 20,
+                   'D': 15}
+
 
 
 
